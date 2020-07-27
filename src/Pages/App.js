232f9a63dc87279c3f1572f94135ac7e9ed4e4c7
高轩云 <yxgstick@gmail.com>
2020-07-27 22:26:55 +0800

    init project

---
+++ src/Pages/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import FindRepeatNumber from "./Array/FindRepeatNumber";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        LeetCode
+        <FindRepeatNumber></FindRepeatNumber>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Tool/Tool.js
@@ -0,0 +1,21 @@
+export const productNumArr = () => {
+  const arrLength = Math.ceil(Math.random() * 10)
+  let res = []
+  console.log('===arrLength===', arrLength)
+  for (let i = 0; i < arrLength; i++) {
+    let item = Math.ceil(Math.random() * 10)
+    res = [item, ...res]
+  }
+  return res
+}
+
+export const findNum = (arr) => {
+  let obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] === undefined) {
+      obj[arr[i]] = i
+    } else {
+      return arr[i]
+    }
+  }
+}
